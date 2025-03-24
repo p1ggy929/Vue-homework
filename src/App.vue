a90c1cc982b9918260cdb2ad0c7242a8638b5fdb
piggy <a3116792638@hotmail.com>
2025-03-24 23:58:20 +0800

    homework

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+// 第一步：引入组件
+import MyComponents from './components/MyComponents.vue';
+</script>
+
+<template>
+  <!--第二步: 显示组件-->
+  <MyComponents/>
+</template>
+
+<style scoped>
+
+</style>
