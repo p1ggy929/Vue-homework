38cc27e7167a85087c14c30e6f0dbe9b55ef07ec
piggy <a3116792638@hotmail.com>
2025-04-21 13:32:46 +0800

    homework

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <button @click="increment">
+      {{ count }}
+    </button>
+    <button @click="state.count++">
+      {{ state.count }}
+    </button>
+    <button @click="counter++">Add 1</button>
+    <p>The button above has been clicked {{ counter }} times.</p>
+    <button @click="greet">Greet</button>
+    <button @click="awesome = !awesome">toggle</button>
+    <h1 v-if="awesome">Vue is awesome!</h1>
+    <h1 v-else>Oh no 😢</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+
+const state = reactive({ count: 0 })
+
+const counter = ref(0)
+
+const name = ref('Vue.js')
+
+function greet(event) {
+  alert(`Hello ${name.value}!`)
+  if (event) {
+    alert(event.target.tagName)
+  }
+}
+
+const awesome = ref(true)
+</script>
---
+++ src/components/MyComponents.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "嘉善卓越班的组件基础"
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">{{ message }}</div>
+</template>
+
+<style>
+
+.container {
+  font-size: 30px;
+  color:brown;
+}
+
+</style>
